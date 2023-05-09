864bb36d78125c77a0293678cd919634576b1743
vyshnaviramisetty <113919689+vyshnaviramisetty@users.noreply.github.com>
2023-05-09 19:36:38 +0530

    Create confusion matrix.r

---
+++ confusion matrix.r
@@ -0,0 +1,5 @@
+set.seed(123)
+data <- data.frame(Actual = sample(c("True","False"), 100, replace = TRUE),
+ PredicƟon = sample(c("True","False"), 100, replace = TRUE)
+)
+table (data$PredicƟon, data$Actual)

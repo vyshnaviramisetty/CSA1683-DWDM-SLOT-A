af5c5f9245d1fb78e5fc427d1dfb3ee05e63c7a1
vyshnaviramisetty <113919689+vyshnaviramisetty@users.noreply.github.com>
2023-05-09 10:10:04 +0530

    Create sub.r

---
+++ sub.r
@@ -0,0 +1,8 @@
+subtract <- function(a, b) {
+  result <- a - b
+  return(result)
+}
+num1 <- as.numeric(readline("Enter the first number: "))
+num2 <- as.numeric(readline("Enter the second number: "))
+difference <- subtract(num1, num2)
+cat("The difference is:", difference)

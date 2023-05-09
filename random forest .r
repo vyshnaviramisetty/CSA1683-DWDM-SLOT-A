e6858711757a8f87e2ae09465cb17c8bc65c523f
vyshnaviramisetty <113919689+vyshnaviramisetty@users.noreply.github.com>
2023-05-09 09:25:06 +0530

    Create random forest .r

---
+++ random forest .r
@@ -0,0 +1,10 @@
+# Load the party package. It will automatically load other
+# required packages.
+library(party)
+library(randomForest)
+
+# Create the forest.
+output.forest <- randomForest(nativeSpeaker ~ age + shoeSize + score, 
+                              data = readingSkills)
+# View the forest results.
+print(output.forest)
